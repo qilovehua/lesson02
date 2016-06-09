d00a72188e9b658fbd1ed6f155b1f137a784c5d7
qilovehua <lhq0631@163.com>
2016-06-09 22:14:56 +0800

    [ADD] load env

---
+++ src/server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import path from 'path'
+import ProjectCore from 'project-core'
+
+const $ = new ProjectCore();
+
+$.init.add((done)=>{
+    // $.config.load(path.resolve(__dirname, 'config.js'));
+    const env = process.env.NODE_ENV || null;
+    if(env){
+        $.config.load(path.resolve(__dirname, '../config', env + '.js'));
+    }
+
+    $.env = env;
+    done();
+});
+
+$.init((err)=>{
+    if(err){
+        console.error(err);
+        process.exit(-1);
+    }else{
+        console.log('inited [env=%s]', $.env);
+    }
+});
